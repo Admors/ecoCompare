43cc67c85ee335f66b3d39adb3e475b9ebcd0c1b
KynanDK <keyskynan@gmail.com>
2025-04-12 10:55:25 +0200

    Added JS & refactored HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
-async function something(){
+async function something() {
     try {
-        const response = await fetch('data/JsonBatiment.json');
+        const response = await fetch("data/JsonBatiment.json");
         const data = await response.json();
-  
-        housesContainer.innerHTML = '<h1>TEST</h1>';
+        
+        housesContainer.innerHTML = "<h1>TEST</h1>";
         console.log("non error");
-      } catch (error) {
+    } catch (error) {
         console.log(error);
-        document.querySelector('.houses-container').innerHTML =  '<p class="text-red-500">Failed to load house data. Please try again later.</p>';
-      }
-  }
-  await something();
+        document.querySelector(".houses-container").innerHTML = "<p class=\"text-red-500\">Failed to load house data. Please try again later.</p>";
+    }
+}
+
+await something();
