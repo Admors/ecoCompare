775def48a1eb4b961da2ca0df24dc40b95551f7b
KynanDK <keyskynan@gmail.com>
2025-04-13 12:20:10 +0200

    Fixed graphs

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,48 +1,44 @@
-async function init2() {
-    try {
-        const response = await fetch("data/municipality.json");
-        const data = await response.json();
-
-        if (data) {
-            const bestCommune = data.results.map(commune => (
-                {name: commune.municipality, espec: commune.e_spec}
-            )).sort((a, b) => a.espec - b.espec).splice(0, 10);
-
-            new Chart(document.querySelector("#chartForBest"), {
-                type: "bar",
-                data: {
-                    labels: bestCommune.map(row => row.name),
-                    datasets: [
-                        {
-                            label: "Annual energy consumption (in kWh/m² per year)",
-                            data: bestCommune.map(row => row.espec),
-                            backgroundColor: "oklch(72.3% 0.219 149.579)"
-                        }
-                    ]
+async function init() {
+    const response = await fetch("data/municipality.json");
+    const data = await response.json();
+    
+    if (!data) return;
+    
+    const bestCommune = data.results.map(commune => (
+        {name: commune.commune, espec: commune.e_spec}
+    )).sort((a, b) => a.espec - b.espec).splice(0, 10);
+    
+    new Chart(document.querySelector("#chartForBest"), {
+        type: "bar",
+        data: {
+            labels: bestCommune.map(row => row.name),
+            datasets: [
+                {
+                    label: "Annual energy consumption (in kWh/m² per year)",
+                    data: bestCommune.map(row => row.espec),
+                    backgroundColor: "oklch(72.3% 0.219 149.579)"
                 }
-            });
-
-            const worstCommune = data.results.map(commune => (
-                {name: commune.municipality, espec: commune.e_spec}
-            )).sort((a, b) => b.espec - a.espec).splice(0, 10);
-
-            new Chart(document.querySelector("#chartForWorst"), {
-                type: "bar",
-                data: {
-                    labels: worstCommune.map(row => row.name),
-                    datasets: [
-                        {
-                            label: "Annual energy consumption (in kWh/m² per year)",
-                            data: worstCommune.map(row => row.espec),
-                            backgroundColor: "oklch(63.7% 0.237 25.331)"
-                        }
-                    ]
-                }
-            });
+            ]
         }
-    } catch (error) {
-        console.log(error)
-    }
+    });
+    
+    const worstCommune = data.results.map(commune => (
+        {name: commune.commune, espec: commune.e_spec}
+    )).sort((a, b) => b.espec - a.espec).splice(0, 10);
+    
+    new Chart(document.querySelector("#chartForWorst"), {
+        type: "bar",
+        data: {
+            labels: worstCommune.map(row => row.name),
+            datasets: [
+                {
+                    label: "Annual energy consumption (in kWh/m² per year)",
+                    data: worstCommune.map(row => row.espec),
+                    backgroundColor: "oklch(63.7% 0.237 25.331)"
+                }
+            ]
+        }
+    });
 }
 
-await init2();
+await init();
